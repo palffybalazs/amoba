d6e0a1af04709cbd51eb60b55cc53228596b404e
palffybalazs <palffybalazs@gmail.com>
2019-05-19 18:53:41 +0200

    All directions are added for winning check

--- gammamester.cpp
+++ gammamester.cpp
@@ -49,30 +49,46 @@ bool gammamester::free_to_click(int v, int f, bool kor)
 bool gammamester::gameover()
 {
     string nyertesszin;
-    for (int i = 0; i<adat.size(); i++)
-    {
-        int stepv = 1;
-        int stepf = 0;
+    vector<koord> iranyok;
+    koord tmp;
+    tmp.v = 1;
+    tmp.f = 0;
+    iranyok.push_back(tmp); //jobbra
+    tmp.v = 1;
+    tmp.f = 1;
+    iranyok.push_back(tmp); //jobbra le
+    tmp.v = 0;
+    tmp.f = 1;
+    iranyok.push_back(tmp); //le
+    tmp.v = 1;
+    tmp.f = -1;
+    iranyok.push_back(tmp);  //jobbra fel
 
-        int hanyvanegysorban = 0;
-        for (int tav = 0; tav<this->hanykellnyerni; tav++)
+    for (int i = 0; i<adat.size(); i++) //minden letett jelbol indulva
+    {
+        for (int g = 0; g<iranyok.size(); g++) //minden iranyba (balra, fel, balra le, balra fel nem kell nezni, ugyanaz mint jobbra, le, jobbra le, stb.)
         {
-            bool vanodarakva = false;
-            for (int h = 0; h<adat.size(); h++)
+            int stepv = iranyok[g].v;
+            int stepf = iranyok[g].f;
+            int hanyvanegysorban = 0;
+            for (int tav = 0; tav<this->hanykellnyerni; tav++)
             {
-                if((adat[h].v == adat[i].v + tav*stepv &&
-                    adat[h].f == adat[i].f + tav*stepf &&
-                    adat[h].kor == adat[i].kor))
+                bool vanodarakva = false;
+                for (int h = 0; h<adat.size(); h++)
                 {
-                    vanodarakva = true;
+                    if((adat[h].v == adat[i].v + tav*stepv &&
+                            adat[h].f == adat[i].f + tav*stepf &&
+                            adat[h].kor == adat[i].kor))
+                    {
+                        vanodarakva = true;
+                    }
+                }
+                if (vanodarakva)
+                {
+                    hanyvanegysorban++;
                 }
             }
-            if (vanodarakva)
-                {
-                   hanyvanegysorban++;
-                }
-        };
-        if(hanyvanegysorban == this->hanykellnyerni)
+            if(hanyvanegysorban == this->hanykellnyerni)
             {
                 this->win = true;
                 if(adat[i].kor)
@@ -84,6 +100,7 @@ bool gammamester::gameover()
                     nyertesszin = "x";
                 }
             }
+        }
     }
 
     if(this->win)
