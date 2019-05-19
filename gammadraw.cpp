9e882dc5092e8aef89c4f2f0fdbb34599a75b6af
palffybalazs <palffybalazs@gmail.com>
2019-05-19 13:54:11 +0200

    style fix

--- gammadraw.cpp
+++ gammadraw.cpp
@@ -37,58 +37,58 @@ gammadraw::gammadraw(string szin, string szam, gammamester* logika)
 void gammadraw::draw()
 {
 
-    for(int j=0;j<meret;j++)
+    for(int j=0; j<meret; j++)
     {
 
-        for(int i=0;i<meret;i++)
-    {
-        x = i*30;
-        gout<<color(255,0,0);
-        gout<<move_to(x,j*30)<<box(30,30);
-        gout<<color(0,0,0);
-        gout<<move_to(x+2,j*30+2)<<box(27,27);
-    }
+        for(int i=0; i<meret; i++)
+        {
+            x = i*30;
+            gout<<color(255,0,0);
+            gout<<move_to(x,j*30)<<box(30,30);
+            gout<<color(0,0,0);
+            gout<<move_to(x+2,j*30+2)<<box(27,27);
+        }
     }
     vector<koord> mostani_allas = logika->mostani_allas();
-    for(int f=0;f<mostani_allas.size();f++)
+    for(int f=0; f<mostani_allas.size(); f++)
     {
         if(loves && mostani_allas[f].kor == false)
-    {
-        if(szin == "kek")
         {
-          gout<<color(0,255,0);
-        }
-        if(szin == "zold")
-        {
-          gout<<color(0,0,255);
-        }
-        gout<<move_to(mostani_allas[f].v*30,mostani_allas[f].f*30);
-        gout<<line_to(mostani_allas[f].v*30+30,mostani_allas[f].f*30+30);
-        gout<<move_to(mostani_allas[f].v*30+30,mostani_allas[f].f*30);
-        gout<<line_to(mostani_allas[f].v*30,mostani_allas[f].f*30+30);
+            if(szin == "kek")
+            {
+                gout<<color(0,255,0);
+            }
+            if(szin == "zold")
+            {
+                gout<<color(0,0,255);
+            }
+            gout<<move_to(mostani_allas[f].v*30,mostani_allas[f].f*30);
+            gout<<line_to(mostani_allas[f].v*30+30,mostani_allas[f].f*30+30);
+            gout<<move_to(mostani_allas[f].v*30+30,mostani_allas[f].f*30);
+            gout<<line_to(mostani_allas[f].v*30,mostani_allas[f].f*30+30);
 
-        //cout<<kor<<endl;
-        //cout<<"nem kor";
-    }
-    if(loves && mostani_allas[f].kor == true)
-    {
-        if(szin == "kek")
-        {
-          gout<<color(0,0,255);
+            //cout<<kor<<endl;
+            //cout<<"nem kor";
         }
-        if(szin == "zold")
+        if(loves && mostani_allas[f].kor == true)
         {
-          gout<<color(0,255,0);
-        }
-        gout<<move_to(mostani_allas[f].v*30,mostani_allas[f].f*30+30);
-        gout<<line_to(mostani_allas[f].v*30+15,mostani_allas[f].f*30);
-        gout<<line_to(mostani_allas[f].v*30+30,mostani_allas[f].f*30+30);
-        gout<<move_to(mostani_allas[f].v*30,mostani_allas[f].f*30+30);
-        gout<<line_to(mostani_allas[f].v*30+30,mostani_allas[f].f*30+30);
-        //cout<<"kor";
-        //cout<<kor<<endl;
+            if(szin == "kek")
+            {
+                gout<<color(0,0,255);
+            }
+            if(szin == "zold")
+            {
+                gout<<color(0,255,0);
+            }
+            gout<<move_to(mostani_allas[f].v*30,mostani_allas[f].f*30+30);
+            gout<<line_to(mostani_allas[f].v*30+15,mostani_allas[f].f*30);
+            gout<<line_to(mostani_allas[f].v*30+30,mostani_allas[f].f*30+30);
+            gout<<move_to(mostani_allas[f].v*30,mostani_allas[f].f*30+30);
+            gout<<line_to(mostani_allas[f].v*30+30,mostani_allas[f].f*30+30);
+            //cout<<"kor";
+            //cout<<kor<<endl;
 
-    }
+        }
     }
 }
 string gammadraw::returnchosen()
@@ -99,9 +99,9 @@ string gammadraw::returnchosen()
 bool gammadraw::handle(genv::event ev, bool focused)
 {
     once = true;
-    for(int k=0;k<=meret;k++)
+    for(int k=0; k<=meret; k++)
     {
-        for(int l=0;l<=meret;l++)
+        for(int l=0; l<=meret; l++)
         {
             if(ev.pos_x>l*30 && ev.pos_x<l*30+30 && ev.pos_y>k*30 && ev.pos_y<(k+1)*30 && ev.button==btn_left)
             {
