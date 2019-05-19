65b5ba841581a9fcffb6d26aefad5271f745143b
palffybalazs <palffybalazs@gmail.com>
2019-05-19 13:04:45 +0200

    unneeded code

--- gammamester.cpp
+++ gammamester.cpp
@@ -31,22 +31,7 @@ gammamester::gammamester(vector<koord>adat)
 
 bool gammamester::handle(genv::event ev, bool focused)
 {
-    for(int i=0;i<adat.size();i++)
-    {
-    fugg = adat[i].f;
-    viz = adat[i].v;
-    for(int k =0;k<5;k++)
-    {
-     for(int j=0;j<adat.size();j++)
-    {
-     if(viz+k == adat[j].v && adat[i].kor==adat[j].kor)
-     {
-         counter++;
-         cout<<counter<<endl;
-     }
-    }
-    }
-    }
+
 
 
 
